94261f4e609e4b8c91932e0cd638e274ae81c08c
francia paucar <134339070+valery-jazminfrancia@users.noreply.github.com>
2023-06-05 13:03:01 -0500

    Add files via upload

---
+++ Dswevaluacion1Application.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.DSWevaluacion1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dsw1Ec1L11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dsw1Ec1L11Application.class, args);
+	}
+
+}
---
+++ Dswevaluacion1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.idat.DSWevaluacion1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dsw1Ec1L11ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
